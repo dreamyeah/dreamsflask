9adfb2f8421e9e523ee28cb7b5720c6f446351a1
hzgeqingyang <hzgeqingyang@corp.netease.com>
2017-04-23 19:46:57 +0800

    fix bug

--- app/models.py
+++ app/models.py
@@ -320,7 +320,7 @@ class Post(db.Model):
     def on_changed_body(target, value, oldvalue, initiator):
         allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
                         'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
-                        'h1', 'h2', 'h3', 'p'，'img']
+                        'h1', 'h2', 'h3', 'p','img']
         target.body_html = bleach.linkify(bleach.clean(
             markdown(value, output_format='html'),
             tags=allowed_tags, strip=True))
