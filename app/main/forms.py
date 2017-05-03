bbbf049177e9d61abad746793a0d447e20869494
hzgeqingyang <hzgeqingyang@corp.netease.com>
2017-05-03 21:42:23 +0800

    user change

--- app/main/forms.py
+++ app/main/forms.py
@@ -15,7 +15,7 @@ class NameForm(Form):
 class EditProfileForm(Form):
     name = StringField('Real name', validators=[Length(0, 64)])
     location = StringField('Location', validators=[Length(0, 64)])
-    about_me = TextAreaField('About me', render_kw={'rows':10})
+    about_me = TextAreaField('About me', render_kw={'rows':15})
     submit = SubmitField('Submit')
 
 
